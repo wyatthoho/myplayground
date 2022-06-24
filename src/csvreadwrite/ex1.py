5d5e225f3e0aef668a77ef07b8d01d92e7afd2f5
Wyatt Ho <wyatthoho@gmail.com>
2022-06-24 11:09:15 +0800

    Feat: has_header, delimeter, skipinitialspace

--- src/csvreadwrite/ex1.py
+++ src/csvreadwrite/ex1.py
@@ -7,9 +7,16 @@ thisdir = os.path.dirname(thispath)
 datafile = 'dataread.csv'
 datapath = os.path.join(thisdir, datafile)
 
+
+# Check has_header
+with open(datapath, 'r') as f:
+    has_header = csv.Sniffer().has_header(f.read())
+    print(has_header)
+
+
 # Read
 with open(datapath, 'r') as f:
-    data = csv.reader(f)
+    data = csv.reader(f, delimiter=',', skipinitialspace=True)
     for rowdata in data:
         print(rowdata)
     
@@ -18,7 +25,7 @@ with open(datapath, 'r') as f:
 
 # Dict reader
 with open(datapath, 'r') as f:
-    data = csv.DictReader(f)
+    data = csv.DictReader(f, delimiter=',', skipinitialspace=True)
     for rowdata in data:
         print(rowdata)
 
