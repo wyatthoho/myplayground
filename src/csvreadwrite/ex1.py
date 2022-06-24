17734a2c27a88f2293b62ce2880abaacbe4a99b6
Wyatt Ho <wyatthoho@gmail.com>
2022-06-24 10:42:28 +0800

    Feat: read csv

--- src/csvreadwrite/ex1.py
+++ src/csvreadwrite/ex1.py
@@ -1,3 +1,27 @@
-'''
-https://www.mcdonalds.com/us/en-us/about-our-food/nutrition-calculator.html
-'''
+import csv
+import os
+
+thispath = os.path.abspath(__file__)
+thisdir = os.path.dirname(thispath)
+
+datafile = 'dataread.csv'
+datapath = os.path.join(thisdir, datafile)
+
+# Read
+with open(datapath, 'r') as f:
+    data = csv.reader(f)
+    for rowdata in data:
+        print(rowdata)
+    
+    print(data.line_num)
+
+
+# Dict reader
+with open(datapath, 'r') as f:
+    data = csv.DictReader(f)
+    for rowdata in data:
+        print(rowdata)
+
+    print(data.fieldnames)
+    print(data.line_num)
+
